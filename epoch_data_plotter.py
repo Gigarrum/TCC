46092bfba0a5e209a0c0e3f2699275a6fd12e0f3
paulo.mausbach <paulo@photon.abtlus.org.br>
2019-10-28 17:00:28 -0300

    Finished data plotter + renamed epochs_data files to correct name

--- epoch_data_plotter.py
+++ epoch_data_plotter.py
@@ -1,4 +1,27 @@
-import pandas
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+datapath = sys.argv[1]
+
+data = pd.read_csv(datapath + 'epochs_data.csv') 
+
+epochs = data['epoch']
+train_losses = data['trainLoss']
+validation_losses = data['validationLoss']
+
+plt.ylabel('LOSS')
+plt.xlabel('EPOCHS')
+plt.xticks(range(0, max(epochs) + 1, 10))
+plt.plot(epochs, train_losses, label='Training')
+plt.legend()
+plt.plot(epochs, validation_losses, label='Validation')
+plt.legend()
+
+#plt.show()
+plt.savefig(datapath + 'lossXepoch_graph.png')
+
+
 
 
 '''
