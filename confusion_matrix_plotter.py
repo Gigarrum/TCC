b3a6f788edb682c7969b0daa962b36f00268349c
paulo.mausbach <paulo@photon.abtlus.org.br>
2019-11-06 16:52:34 -0300

    Due to problems on plotting confusion matrix using matoplotlib on python script, create an jupyter notebook script for this

--- confusion_matrix_plotter.py
+++ confusion_matrix_plotter.py
@@ -36,88 +36,59 @@ def id_to_class(ids):
 
   return ids
 
-
-def plot_confusion_matrix(cm,
-                          target_names,
-                          title='Confusion matrix',
-                          cmap=None,
-                          normalize=True):
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
     """
-    given a sklearn confusion matrix (cm), make a nice plot
-
-    Arguments
-    ---------
-    cm:           confusion matrix from sklearn.metrics.confusion_matrix
-
-    target_names: given classification classes such as [0, 1, 2]
-                  the class names, for example: ['high', 'medium', 'low']
-
-    title:        the text to display at the top of the matrix
-
-    cmap:         the gradient of the values displayed from matplotlib.pyplot.cm
-                  see http://matplotlib.org/examples/color/colormaps_reference.html
-                  plt.get_cmap('jet') or plt.cm.Blues
-
-    normalize:    If False, plot the raw numbers
-                  If True, plot the proportions
-
-    Usage
-    -----
-    plot_confusion_matrix(cm           = cm,                  # confusion matrix created by
-                                                              # sklearn.metrics.confusion_matrix
-                          normalize    = True,                # show proportions
-                          target_names = y_labels_vals,       # list of names of the classes
-                          title        = best_estimator_name) # title of graph
-
-    Citiation
-    ---------
-    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
-
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
     """
-    import matplotlib.pyplot as plt
-    import numpy as np
-    import itertools
-
-    accuracy = np.trace(cm) / float(np.sum(cm))
-    misclass = 1 - accuracy
-
-    if cmap is None:
-        cmap = plt.get_cmap('Blues')
-
-    plt.figure(figsize=(8, 6))
-    plt.imshow(cm,interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-
-    if target_names is not None:
-        tick_marks = np.arange(len(target_names))
-        plt.xticks(tick_marks, target_names, rotation=45)
-        plt.yticks(tick_marks, target_names)
-
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    #classes = classes[unique_labels(y_true, y_pred)]
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        if normalize:
-            plt.text(j, i, "{:0.2f}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-        else:
-            plt.text(j, i, "{:,}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-  
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
-    return plt
-
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+    print(cm)
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+    ax.set_ylim(len(cm)-0.5, -0.5) #Bug correction for matplotlib 3.1.1 version
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    #fig.tight_layout()
+    return ax
 
 if __name__ == "__main__":
-
+  
   classes = ['ALA','ARG','ASN','ASP','CYS','GLU','GLN','GLY','HIS','ILE','LEU','LYS','MET','PHE','PRO','SER','THR','TRP','TYR','VAL']
-
+  '''
   trainset_path = sys.argv[1]
   weights_path = sys.argv[2]
 
@@ -141,13 +112,13 @@ if __name__ == "__main__":
   #Load Net model
   model = TheNet()
   model.load_state_dict(torch.load(weights_path + 'weights.pth',map_location='cpu'))
-
+  
   print('Neural network evaluation mode activated!!!')
   model.eval()
-
+  '''
   all_labels = []
   all_predictions = []
-
+  '''
   with torch.no_grad():
       for data in trainloader:
           grids, labels, resIdx = data
@@ -159,20 +130,17 @@ if __name__ == "__main__":
           print(output)
 
           #_, predicted = torch.max(output.data, 1)
+  '''
+
+  all_labels = np.random.randint(20, size=256)
+  all_predictions = np.random.randint(20, size=256)
 
   cm = confusion_matrix(all_labels, all_predictions)
-  '''
-  skplt.metrics.plot_confusion_matrix(all_labels,all_predictions,normalize=True,figsize=(12,20))
-  plt.show()
-  #plt.savefig(weights_path + 'confusionMatrix.png')
-  '''
-
-  print(cm)
-  plot = plot_confusion_matrix(cm,target_names=classes)
-  plot.show()
-  #plot.savefig(weights_path + 'confusionMatrix.png')
   
-
+  print(cm)
+  ax = plot_confusion_matrix(all_labels,all_predictions,classes,normalize=True,title='Confusion Matrix')
+  #plot.show()
+  plt.savefig('confusionMatrix.png',figure_size=(12,12))
 
  #python confusion_matrix_plotter.py C:\Users\pbmau\Documents\Paulo\Faculdade\TCC\database\test_sampling\ C:\Users\pbmau\Documents\Paulo\Faculdade\TCC\System\experiments\TheNet-arch1\TheNet-db=10%-batch=256-Adam-lr=0,0001-mm=0,9-epcs=100\         
 
